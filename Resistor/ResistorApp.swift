ab7d70c2ac9f0cb35f698e918965f2205919f44f
himynameisjimmy <prahalad.prithivathi@gmail.com>
2025-02-11 20:16:32 -0600

    Initial Commit

---
+++ Resistor/ResistorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResistorApp.swift
+//  Resistor
+//
+//  Created by Prahalad on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ResistorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
